aaf49233e16fbae73c5fd3a4120785825940167b
DaniFoldi <dani@stefan.hu>
2018-06-17 17:54:28 +0200

    Added [free] chests

--- AIO-Plugin/src/AIO/aio.java
+++ AIO-Plugin/src/AIO/aio.java
@@ -18,6 +18,7 @@ import org.bukkit.event.entity.ItemDespawnEvent;
 import org.bukkit.event.entity.ItemMergeEvent;
 import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
@@ -649,6 +650,50 @@ public class aio extends JavaPlugin implements Listener {
 				Block sign = (Block)chest.getRelative(BlockFace.SOUTH);
 				if (((Sign)sign.getState()).getLines()[1].equals("[Trash]") && sign.getData() == (byte)2) {
 					event.getInventory().clear();
+				}
+			}
+		}
+	}
+	
+	@EventHandler
+	private void freeChest(InventoryOpenEvent event) {
+		if (event.getInventory().getType() == InventoryType.CHEST) {
+			Block chest = event.getInventory().getLocation().getBlock();
+			if (chest.getRelative(BlockFace.EAST).getType() == Material.WALL_SIGN) {
+				Block sign = (Block)chest.getRelative(BlockFace.EAST);
+				Sign signBlock = (Sign)sign.getState();
+				if (((Sign)sign.getState()).getLines()[1].equals("[Free]") && sign.getData() == (byte)5) {
+					Material material = Material.matchMaterial(((Sign)sign.getState()).getLines()[2]);
+					for (int i = 0; i < 27; i++) {
+						event.getInventory().setItem(i, new ItemStack(material, material.getMaxStackSize()));
+					}
+				}
+			}
+			if (chest.getRelative(BlockFace.WEST).getType() == Material.WALL_SIGN) {
+				Block sign = (Block)chest.getRelative(BlockFace.WEST);
+				if (((Sign)sign.getState()).getLines()[1].equals("[Free]") && sign.getData() == (byte)4) {
+					Material material = Material.matchMaterial(((Sign)sign.getState()).getLines()[2]);
+					for (int i = 0; i < 27; i++) {
+						event.getInventory().setItem(i, new ItemStack(material, material.getMaxStackSize()));
+					}
+				}
+			}
+			if (chest.getRelative(BlockFace.NORTH).getType() == Material.WALL_SIGN) {
+				Block sign = (Block)chest.getRelative(BlockFace.NORTH);
+				if (((Sign)sign.getState()).getLines()[1].equals("[Free]") && sign.getData() == (byte)3) {
+					Material material = Material.matchMaterial(((Sign)sign.getState()).getLines()[2]);
+					for (int i = 0; i < 27; i++) {
+						event.getInventory().setItem(i, new ItemStack(material, material.getMaxStackSize()));
+					}
+				}
+			}
+			if (chest.getRelative(BlockFace.SOUTH).getType() == Material.WALL_SIGN) {
+				Block sign = (Block)chest.getRelative(BlockFace.SOUTH);
+				if (((Sign)sign.getState()).getLines()[1].equals("[Free]") && sign.getData() == (byte)2) {
+					Material material = Material.matchMaterial(((Sign)sign.getState()).getLines()[2]);
+					for (int i = 0; i < 27; i++) {
+						event.getInventory().setItem(i, new ItemStack(material, material.getMaxStackSize()));
+					}
 				}
 			}
 		}
