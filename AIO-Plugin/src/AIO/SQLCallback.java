2ae3ea061bf73461bb537dd52c2a4b4e16aa0ebf
DaniFoldi <dani@stefan.hu>
2018-06-25 23:20:58 +0200

    Added an sql callback object

---
+++ AIO-Plugin/src/AIO/SQLCallback.java
@@ -0,0 +1,20 @@
+package AIO;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.sql.ResultSet;
+
+public class SQLCallback extends BukkitRunnable {
+    @Override
+    public void run() {
+
+    }
+
+    public void callback(ResultSet result) {
+
+    }
+
+    public void callback() {
+
+    }
+}
