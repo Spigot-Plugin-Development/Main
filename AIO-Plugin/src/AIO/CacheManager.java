33a7a20137dfb198e38951b6db5c8a76966fb15a
DaniFoldi <dani@stefan.hu>
2018-06-25 23:23:49 +0200

    Player cache handling improved

--- AIO-Plugin/src/AIO/CacheManager.java
+++ AIO-Plugin/src/AIO/CacheManager.java
@@ -1,8 +1,5 @@
 package AIO;
 
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
-
 import java.sql.ResultSet;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,40 +7,60 @@ import java.util.UUID;
 
 import static org.bukkit.Bukkit.getServer;
 
-public class CacheManager extends Callable<PlayerInfo{
+public class CacheManager {
 
     aio plugin;
 
-    Map<Player, PlayerInfo> cachedPlayers = new HashMap<Player, PlayerInfo>();
+    Map<UUID, PlayerInfo> cachedPlayers = new HashMap<UUID, PlayerInfo>();
 
     CacheManager(aio plugin) {
         this.plugin = plugin;
     }
 
+    public void addPlayer(PlayerInfo info) {
+        cachedPlayers.put(info.uuid, info);
+    }
+
+    public boolean containsPlayer(UUID uuid) {
+        return cachedPlayers.containsKey(uuid);
+    }
+
     public PlayerInfo getPlayer(UUID uuid) {
+        return cachedPlayers.get(uuid);
+    }
+
+    public void setPlayer(UUID uuid, PlayerInfo info) {
+        cachedPlayers.replace(uuid, info);
+    }
+
+    public void removePlayer(UUID uuid) {
+        cachedPlayers.remove(uuid);
+    }
+
+    public void playerJoin(UUID uuid) {
         plugin.sqlconnector.query("SELECT * FROM minecraft_player WHERE minecraft_player_UUID='" + uuid.toString() + "';", new SQLCallback() {
             @Override
-            public Object callback(ResultSet result) {
+            public void callback(ResultSet result) {
                 try {
                     if (result.next()) {
-                        cachedPlayers.put(getServer().getPlayer(uuid), new PlayerInfo(uuid,
-                            result.getString("minecraft_player_name"),
-                            result.getTimestamp("minecraft_player_last_join"),
-                            result.getTimestamp("minecraft_player_last_quit"),
-                            result.getString("minecraft_player_last_ip"),
-                            result.getString("minecraft_player_server"),
-                            Convert.StringToLocation(result.getString("minecraft_player_location")),
-                            result.getInt("minecraft_player_gamemode"),
-                            result.getBoolean("minecraft_player_god_mode"),
-                            result.getBoolean("minecraft_player_fly"),
-                            result.getBoolean("minecraft_player_frozen"),
-                            result.getBoolean("minecraft_player_vanished"),
-                            result.getDate("minecraft_player_muted"),
-                            result.getDate("minecraft_player_banned"),
-                            result.getInt("minecraft_player_coins"),
-                            result.getString("minecraft_player_nick"),
-                            result.getInt("minecraft_player_balance"),
-                            result.getString("minecaft_player_warnings")
+                        cachedPlayers.put(uuid, new PlayerInfo(uuid,
+                                result.getString("minecraft_player_name"),
+                                result.getTimestamp("minecraft_player_last_join"),
+                                result.getTimestamp("minecraft_player_last_quit"),
+                                result.getString("minecraft_player_last_ip"),
+                                result.getString("minecraft_player_server"),
+                                Convert.StringToLocation(result.getString("minecraft_player_location")),
+                                result.getInt("minecraft_player_gamemode"),
+                                result.getBoolean("minecraft_player_god_mode"),
+                                result.getBoolean("minecraft_player_fly"),
+                                result.getBoolean("minecraft_player_frozen"),
+                                result.getBoolean("minecraft_player_vanished"),
+                                result.getDate("minecraft_player_muted"),
+                                result.getDate("minecraft_player_banned"),
+                                result.getInt("minecraft_player_coins"),
+                                result.getString("minecraft_player_nick"),
+                                result.getInt("minecraft_player_balance"),
+                                result.getString("minecaft_player_warnings")
                         ));
                     }
                 } catch (Exception e) {
@@ -51,6 +68,38 @@ public class CacheManager extends Callable<PlayerInfo{
                 }
             }
         });
-        return null;
+    }
+
+    public void playerQuit(UUID uuid) {
+        plugin.sqlconnector.update("SELECT * FROM minecraft_player WHERE minecraft_player_UUID='" + uuid.toString() + "';", new SQLCallback() {
+            @Override
+            public void callback(ResultSet result) {
+                try {
+                    if (result.next()) {
+                        cachedPlayers.put(uuid, new PlayerInfo(uuid,
+                                result.getString("minecraft_player_name"),
+                                result.getTimestamp("minecraft_player_last_join"),
+                                result.getTimestamp("minecraft_player_last_quit"),
+                                result.getString("minecraft_player_last_ip"),
+                                result.getString("minecraft_player_server"),
+                                Convert.StringToLocation(result.getString("minecraft_player_location")),
+                                result.getInt("minecraft_player_gamemode"),
+                                result.getBoolean("minecraft_player_god_mode"),
+                                result.getBoolean("minecraft_player_fly"),
+                                result.getBoolean("minecraft_player_frozen"),
+                                result.getBoolean("minecraft_player_vanished"),
+                                result.getDate("minecraft_player_muted"),
+                                result.getDate("minecraft_player_banned"),
+                                result.getInt("minecraft_player_coins"),
+                                result.getString("minecraft_player_nick"),
+                                result.getInt("minecraft_player_balance"),
+                                result.getString("minecaft_player_warnings")
+                        ));
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
--- AIO-Plugin/src/AIO/PlayerJoin.java
+++ AIO-Plugin/src/AIO/PlayerJoin.java
@@ -2,20 +2,72 @@ package AIO;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.sql.ResultSet;
+import java.util.UUID;
 
 public class PlayerJoin implements Listener {
 	
-	private Plugin plugin;
+	private aio plugin;
 	
-	PlayerJoin(Plugin plugin) {
+	PlayerJoin(aio plugin) {
 		this.plugin = plugin;
 	}
 	
 	@EventHandler
 	public void onJoin(PlayerJoinEvent event) {
+		if (plugin.cacheManager.containsPlayer(event.getPlayer().getUniqueId())) {
+			PlayerInfo info = plugin.cacheManager.getPlayer(event.getPlayer().getUniqueId());
+			if (info.godMode) {
+				plugin.godPlayers.add(event.getPlayer());
+			}
+
+		}
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+
+			}
+		}.runTaskTimer(plugin, 1, 1);
 		event.setJoinMessage(event.getPlayer().getDisplayName().toString() + " has joined the game. Welcome!");
 		event.getPlayer().sendMessage("Welcome " + event.getPlayer().getDisplayName().toString() + " to the game");
+	}
+
+	@EventHandler
+	public void beforeJoin(AsyncPlayerPreLoginEvent event) {
+		UUID uuid = event.getUniqueId();
+		plugin.sqlconnector.query("SELECT * FROM minecraft_player WHERE minecraft_player_UUID = '" + uuid + "';", new SQLCallback() {
+			@Override
+			public void callback(ResultSet result) {
+				try {
+					if (result.next()) {
+						plugin.cacheManager.addPlayer(new PlayerInfo(uuid,
+								result.getString("minecraft_player_name"),
+								result.getTimestamp("minecraft_player_last_join"),
+								result.getTimestamp("minecraft_player_last_quit"),
+								result.getString("minecraft_player_last_ip"),
+								result.getString("minecraft_player_server"),
+								Convert.StringToLocation(result.getString("minecraft_player_location")),
+								result.getInt("minecraft_player_gamemode"),
+								result.getBoolean("minecraft_player_god_mode"),
+								result.getBoolean("minecraft_player_fly"),
+								result.getBoolean("minecraft_player_frozen"),
+								result.getBoolean("minecraft_player_vanished"),
+								result.getDate("minecraft_player_muted"),
+								result.getDate("minecraft_player_banned"),
+								result.getInt("minecraft_player_coins"),
+								result.getString("minecraft_player_nick"),
+								result.getInt("minecraft_player_balance"),
+								result.getString("minecaft_player_warnings")));
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 }
