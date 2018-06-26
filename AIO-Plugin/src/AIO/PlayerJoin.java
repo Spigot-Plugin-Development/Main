8d364692c6be68324caa6051681eb456aa709d42
DaniFoldi <dani@stefan.hu>
2018-06-26 21:30:12 +0200

    Player cache now working

--- AIO-Plugin/src/AIO/PlayerJoin.java
+++ AIO-Plugin/src/AIO/PlayerJoin.java
@@ -1,6 +1,7 @@
 package AIO;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
@@ -8,6 +9,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.sql.ResultSet;
+import java.util.Date;
 import java.util.UUID;
 
 public class PlayerJoin implements Listener {
@@ -23,19 +25,70 @@ public class PlayerJoin implements Listener {
 	public void onJoin(PlayerJoinEvent event) {
 		if (plugin.cacheManager.containsPlayer(event.getPlayer().getUniqueId())) {
 			PlayerInfo info = plugin.cacheManager.getPlayer(event.getPlayer().getUniqueId());
+			if (info.banned.after(new Date())) {
+				event.getPlayer().kickPlayer("You are banned!");
+			}
+			event.getPlayer().teleport(info.location);
+			event.getPlayer().setGameMode(Convert.IntToGamemode(info.gamemode));
 			if (info.godMode) {
 				plugin.godManager.addGod(event.getPlayer());
 			}
-
-		}
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-
+			if (info.fly) {
+				plugin.flyManager.addFly(event.getPlayer());
 			}
-		}.runTaskTimer(plugin, 1, 1);
-		event.setJoinMessage(event.getPlayer().getDisplayName().toString() + " has joined the game. Welcome!");
-		event.getPlayer().sendMessage("Welcome " + event.getPlayer().getDisplayName().toString() + " to the game");
+			if (info.frozen) {
+				plugin.freezeManager.addFrozen(event.getPlayer());
+			}
+			if (info.vanished) {
+				plugin.vanishManager.addVanish(event.getPlayer());
+			} else {
+                event.setJoinMessage(event.getPlayer().getDisplayName() + " has joined the game.");
+            }
+			if (new Date().before(info.muted)) {
+				plugin.playerMessage.mutePlayer(event.getPlayer());
+			}
+			plugin.economyManager.set(event.getPlayer().getUniqueId(), info.balance);
+			if (info.lastQuit.equals(info.lastJoin)) {
+                event.setJoinMessage("Welcome " + event.getPlayer().getDisplayName() + " to the server!");
+            }
+		} else {
+		    event.setJoinMessage("");
+		    new BukkitRunnable() {
+                @Override
+                public void run() {
+                    if (plugin.cacheManager.containsPlayer(event.getPlayer().getUniqueId())) {
+                        PlayerInfo info = plugin.cacheManager.getPlayer(event.getPlayer().getUniqueId());
+                        if (new Date().before(info.banned)) {
+                            event.getPlayer().kickPlayer("You are banned!");
+                        }
+                        event.getPlayer().teleport(info.location);
+                        event.getPlayer().setGameMode(Convert.IntToGamemode(info.gamemode));
+                        if (info.godMode) {
+                            plugin.godManager.addGod(event.getPlayer());
+                        }
+                        if (info.fly) {
+                            plugin.flyManager.addFly(event.getPlayer());
+                        }
+                        if (info.frozen) {
+                            plugin.freezeManager.addFrozen(event.getPlayer());
+                        }
+                        if (info.vanished) {
+                            plugin.vanishManager.addVanish(event.getPlayer());
+                        } else {
+                            event.setJoinMessage(event.getPlayer().getDisplayName() + " has joined the game.");
+                        }
+                        if (new Date().before(info.muted)) {
+                            plugin.playerMessage.mutePlayer(event.getPlayer());
+                        }
+                        plugin.economyManager.set(event.getPlayer().getUniqueId(), info.balance);
+                        if (info.lastQuit.equals(info.lastJoin)) {
+                            event.setJoinMessage("Welcome " + event.getPlayer().getDisplayName() + " to the server!");
+                        }
+                        cancel();
+                    }
+                }
+            }.runTaskTimer(plugin, 5, 1);
+        }
 	}
 
 	@EventHandler
@@ -63,7 +116,32 @@ public class PlayerJoin implements Listener {
 								result.getInt("minecraft_player_coins"),
 								result.getString("minecraft_player_nick"),
 								result.getInt("minecraft_player_balance"),
-								result.getString("minecaft_player_warnings")));
+								result.getString("minecraft_player_warnings")));
+					} else {
+					    plugin.cacheManager.addPlayer(new PlayerInfo(uuid));
+					    PlayerInfo info = plugin.cacheManager.getPlayer(event.getUniqueId());
+                        plugin.sqlconnector.update("INSERT INTO minecraft_player (" +
+                                "minecraft_player_UUID, minecraft_player_name, minecraft_player_last_join, minecraft_player_last_quit, minecraft_player_last_ip, minecraft_player_server, minecraft_player_location, " +
+                                "minecraft_player_gamemode, minecraft_player_god_mode, minecraft_player_fly, minecraft_player_frozen, minecraft_player_vanished, minecraft_player_muted, " +
+                                "minecraft_player_banned, minecraft_player_coins, minecraft_player_nick, minecraft_player_balance, minecraft_player_warnings) VALUES (" +
+                                "'" + info.uuid + "', " +
+                                "'" + info.name + "', " +
+                                "'" + Convert.DateToString(new Date()) + "', " +
+                                "'" + Convert.DateToString(new Date()) + "', " +
+                                "'" + event.getAddress().toString() + "', " +
+                                "'" + plugin.getServer().getName() + "', " +
+                                "'" + Convert.LocationToString(info.location) + "', " +
+                                "'" + info.gamemode + "',  " +
+                                "'" + Convert.BooleanToInt(info.godMode) + "', " +
+                                "'" + Convert.BooleanToInt(info.fly) + "', " +
+                                "'" + Convert.BooleanToInt(info.frozen) + "', " +
+                                "'" + Convert.BooleanToInt(info.vanished) + "', " +
+                                "'" + Convert.DateToString(info.muted) + "', " +
+                                "'" + Convert.DateToString(info.banned) + "', " +
+                                "'" + info.coins + "', " +
+                                "'" + info.nick + "', " +
+                                "'" + info.balance + "', " +
+                                "'" + info.warnings + "');", new SQLCallback());
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
--- AIO-Plugin/src/AIO/PlayerLeave.java
+++ AIO-Plugin/src/AIO/PlayerLeave.java
@@ -5,6 +5,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import java.util.Date;
+
 public class PlayerLeave implements Listener {
 	
 	private aio plugin;
@@ -16,6 +18,33 @@ public class PlayerLeave implements Listener {
 	
 	@EventHandler
 	public void onLeave(PlayerQuitEvent event) {
-		event.setQuitMessage(event.getPlayer().getDisplayName().toString() + " has left the game.");
+		if (!plugin.vanishManager.isVanished(event.getPlayer())) {
+			event.setQuitMessage(event.getPlayer().getDisplayName().toString() + " has left the game.");
+		} else {
+			event.setQuitMessage("");
+		}
+		plugin.sqlconnector.update("UPDATE minecraft_player SET " +
+				"minecraft_player_name = '" + event.getPlayer().getName() + "'," +
+				"minecraft_player_last_quit = '" + Convert.DateToString(new Date()) + "'," +
+				"minecraft_player_last_ip = '" + event.getPlayer().getAddress().toString() + "'," +
+				"minecraft_player_server = '" + plugin.getServer().getName() + "'," +
+				"minecraft_player_location = '" + Convert.LocationToString(event.getPlayer().getLocation()) + "'," +
+				"minecraft_player_gamemode = '" + Convert.GamemodeToInt(event.getPlayer().getGameMode()) + "'," +
+				"minecraft_player_god_mode = '" + Convert.BooleanToInt(plugin.godManager.isGod(event.getPlayer())) + "'," +
+				"minecraft_player_fly = '" + Convert.BooleanToInt(plugin.flyManager.canFly(event.getPlayer())) + "'," +
+				"minecraft_player_frozen = '" + Convert.BooleanToInt(plugin.freezeManager.isFrozen(event.getPlayer())) + "'," +
+				"minecraft_player_vanished = '" + Convert.BooleanToInt(plugin.vanishManager.isVanished(event.getPlayer())) + "'," +
+				"minecraft_player_muted = '" + Convert.DateToString(plugin.cacheManager.getPlayer(event.getPlayer().getUniqueId()).muted) + "'," +
+				"minecraft_player_banned = '" + Convert.DateToString(plugin.cacheManager.getPlayer(event.getPlayer().getUniqueId()).banned) + "'," +
+				"minecraft_player_coins = '" + plugin.cacheManager.getPlayer(event.getPlayer().getUniqueId()).coins + "'," +
+				"minecraft_player_nick = '" + event.getPlayer().getDisplayName() + "'," +
+				"minecraft_player_balance = '" + plugin.economyManager.getBalance(event.getPlayer().getUniqueId()) + "'," +
+				"minecraft_player_warnings = '" + plugin.cacheManager.getPlayer(event.getPlayer().getUniqueId()).warnings + "'" +
+				" WHERE minecraft_player_UUID = '" + event.getPlayer().getUniqueId() + "';", new SQLCallback() {
+			@Override
+			public void callback() {
+				plugin.cacheManager.removePlayer(event.getPlayer().getUniqueId());
+			}
+		});
 	}
 }
