b2cb9644f39d77bb08bcf420b8433d5feca828a7
Ádám András <adm.andras@gmail.com>
2018-06-30 15:03:06 +0200

    chat update

--- AIO-Plugin/src/AIO/PlayerMessage.java
+++ AIO-Plugin/src/AIO/PlayerMessage.java
@@ -44,19 +44,19 @@ public class PlayerMessage implements Listener {
 
         //GROUP NAMES ARE TEMPORARY
         if(plugin.permission.getPrimaryGroup(event.getPlayer()).equalsIgnoreCase("Owner")) {
-            event.setFormat(aio.colorize("&7[&9Owner&7] &9" + playerName + "&7 >> &f" + event.getMessage()));
+            event.setFormat(aio.colorize("&7[&9Owner&7] &9" + playerName + "&7 » &f" + event.getMessage()));
         } else if(plugin.permission.getPrimaryGroup(event.getPlayer()).equalsIgnoreCase("Admin")) {
-            event.setFormat(aio.colorize("&7[&cAdmin&7] &c" + playerName + "&7 >> &f" + event.getMessage()));
+            event.setFormat(aio.colorize("&7[&cAdmin&7] &c" + playerName + "&7 » &f" + event.getMessage()));
         } else if(plugin.permission.getPrimaryGroup(event.getPlayer()).equalsIgnoreCase("Legend")) {
-            event.setFormat(aio.colorize("&7[&6Legend&7] &6" + playerName + "&7 >> &f" + event.getMessage()));
+            event.setFormat(aio.colorize("&7[&6Legend&7] &6" + playerName + "&7 » &f" + event.getMessage()));
         } else if(plugin.permission.getPrimaryGroup(event.getPlayer()).equalsIgnoreCase("King")) {
-            event.setFormat(aio.colorize("&7[&3King&7] &3" + playerName + "&7 >> &f" + event.getMessage()));
+            event.setFormat(aio.colorize("&7[&3King&7] &3" + playerName + "&7 » &f" + event.getMessage()));
         } else if(plugin.permission.getPrimaryGroup(event.getPlayer()).equalsIgnoreCase("Premium")) {
-            event.setFormat(aio.colorize("&7[&5Premium&7] &5" + playerName + "&7 >> &f" + event.getMessage()));
+            event.setFormat(aio.colorize("&7[&5Premium&7] &5" + playerName + "&7 » &f" + event.getMessage()));
         } else if(plugin.permission.getPrimaryGroup(event.getPlayer()).equalsIgnoreCase("Vip")) {
-            event.setFormat(aio.colorize("&7[&aV.I.P.&7] &a" + playerName + "&7 >> &f" + event.getMessage()));
+            event.setFormat(aio.colorize("&7[&aV.I.P.&7] &a" + playerName + "&7 » &f" + event.getMessage()));
         } else {
-            event.setFormat(aio.colorize("&7[&fDefault&7] &f" + playerName + "&7 >> &f" + event.getMessage()));
+            event.setFormat(aio.colorize("&7[&fDefault&7] &f" + playerName + "&7 » &f" + event.getMessage()));
         }
 	}
 }
