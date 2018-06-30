2c95486ff6cd2b5e9bbc57db76392cd138a57c65
Ádám András <adm.andras@gmail.com>
2018-06-30 13:29:45 +0200

    playerMessage updated
    Chat modification added, works with nick

--- AIO-Plugin/src/AIO/PlayerMessage.java
+++ AIO-Plugin/src/AIO/PlayerMessage.java
@@ -31,8 +31,32 @@ public class PlayerMessage implements Listener {
 	@EventHandler
 	public void onMessage(AsyncPlayerChatEvent event) {
 		if (mutedPlayers.contains(event.getPlayer())) {
-			event.setCancelled(true);
-		}
-		event.setFormat(event.getPlayer().getDisplayName() + aio.colorize("&5&l > &r") + aio.colorize(event.getMessage()));
+            event.setCancelled(true);
+        }
+
+        String playerName;
+
+        if(event.getPlayer().getDisplayName().equalsIgnoreCase("")) {
+            playerName = event.getPlayer().getName();
+        } else {
+            playerName = event.getPlayer().getDisplayName();
+        }
+
+        //GROUP NAMES ARE TEMPORARY
+        if(plugin.permission.getPrimaryGroup(event.getPlayer()).equalsIgnoreCase("Owner")) {
+            event.setFormat(aio.colorize("&7[&9Owner&7] &9" + playerName + "&7 >> &f" + event.getMessage()));
+        } else if(plugin.permission.getPrimaryGroup(event.getPlayer()).equalsIgnoreCase("Admin")) {
+            event.setFormat(aio.colorize("&7[&cAdmin&7] &c" + playerName + "&7 >> &f" + event.getMessage()));
+        } else if(plugin.permission.getPrimaryGroup(event.getPlayer()).equalsIgnoreCase("Legend")) {
+            event.setFormat(aio.colorize("&7[&6Legend&7] &6" + playerName + "&7 >> &f" + event.getMessage()));
+        } else if(plugin.permission.getPrimaryGroup(event.getPlayer()).equalsIgnoreCase("King")) {
+            event.setFormat(aio.colorize("&7[&3King&7] &3" + playerName + "&7 >> &f" + event.getMessage()));
+        } else if(plugin.permission.getPrimaryGroup(event.getPlayer()).equalsIgnoreCase("Premium")) {
+            event.setFormat(aio.colorize("&7[&5Premium&7] &5" + playerName + "&7 >> &f" + event.getMessage()));
+        } else if(plugin.permission.getPrimaryGroup(event.getPlayer()).equalsIgnoreCase("Vip")) {
+            event.setFormat(aio.colorize("&7[&aV.I.P.&7] &a" + playerName + "&7 >> &f" + event.getMessage()));
+        } else {
+            event.setFormat(aio.colorize("&7[&fDefault&7] &f" + playerName + "&7 >> &f" + event.getMessage()));
+        }
 	}
 }
