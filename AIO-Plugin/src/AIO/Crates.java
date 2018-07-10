f87e7d55d9172cb015af05fe53678986d0dade54
DaniFoldi <dani@stefan.hu>
2018-07-10 10:17:48 +0200

    Fixed crate nulls

--- AIO-Plugin/src/AIO/Crates.java
+++ AIO-Plugin/src/AIO/Crates.java
@@ -12,6 +12,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -77,7 +78,13 @@ public class Crates implements Listener, CommandExecutor {
 
     @EventHandler(priority=EventPriority.HIGH)
     public void rightClick(PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
         if (event.getClickedBlock().getType() != Material.CHEST) {
+            return;
+        }
+        if (event.getItem() == null) {
             return;
         }
         if (event.getItem().getType() != Material.TRIPWIRE_HOOK) {
