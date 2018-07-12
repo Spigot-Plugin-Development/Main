2a720da99e6285f6a4268790f17a14a83f002e15
Ádám András <adm.andras@gmail.com>
2018-07-12 12:56:22 +0200

    WeatherManager UX Update

--- AIO-Plugin/src/AIO/WeatherManager.java
+++ AIO-Plugin/src/AIO/WeatherManager.java
@@ -21,99 +21,84 @@ public class WeatherManager implements CommandExecutor {
         if(command.getName().equalsIgnoreCase("sun")) {
             if(sender instanceof Player) {
                 if(!sender.hasPermission("aio.weather")) {
-                    sender.sendMessage("You don't have permission to execute this command.");
-                    return false;
+                    sender.sendMessage(plugin.getMessage("messages.no_permission"));
+                    return true;
                 }
                 if(args.length == 0) {
                     ((Player) sender).getLocation().getWorld().setThundering(false);
                     ((Player) sender).getLocation().getWorld().setStorm(false);
-                    sender.sendMessage("Weather set to sunny!");
-                    return false;
+                    sender.sendMessage(plugin.getMessage("weather.set1", "sunny"));
+                    return true;
                 }
             }
-            if(args.length == 1) {
-                if(plugin.getServer().getWorld(args[0]) != null) {
-                    plugin.getServer().getWorld(args[0]).setThundering(false);
-                    plugin.getServer().getWorld(args[0]).setStorm(false);
-                    sender.sendMessage("Weather set to sunny in " + plugin.getServer().getWorld(args[0]).getName() + "!");
-                    return false;
-                } else {
-                    sender.sendMessage("World not found.");
-                    return false;
-                }
-            } else if(args.length == 0) {
-                sender.sendMessage("World not given.");
-                return false;
-            } else {
-                sender.sendMessage("Too many arguments.");
-                return false;
+            if(args.length == 0) {
+                sender.sendMessage(plugin.getMessage("weather.usage", "sun"));
+                return true;
             }
+            if(plugin.getServer().getWorld(args[0]) == null) {
+                sender.sendMessage(plugin.getMessage("weather.world_not_found", args[0]));
+                return true;
+            }
+            plugin.getServer().getWorld(args[0]).setThundering(false);
+            plugin.getServer().getWorld(args[0]).setStorm(false);
+            sender.sendMessage(plugin.getMessage("weather.set2", "sunny", args[0]));
+            return true;
         }
 
         //Set weather to rainy
         if(command.getName().equalsIgnoreCase("rain")) {
             if(sender instanceof Player) {
                 if(!sender.hasPermission("aio.weather")) {
-                    sender.sendMessage("You don't have permission to execute this command.");
-                    return false;
+                    sender.sendMessage(plugin.getMessage("messages.no_permission"));
+                    return true;
                 }
                 if(args.length == 0) {
                     ((Player) sender).getLocation().getWorld().setThundering(false);
                     ((Player) sender).getLocation().getWorld().setStorm(true);
-                    sender.sendMessage("Weather set to rainy!");
-                    return false;
+                    sender.sendMessage(plugin.getMessage("weather.set1", "rainy"));
+                    return true;
                 }
             }
-            if(args.length == 1) {
-                if(plugin.getServer().getWorld(args[0]) != null) {
-                    plugin.getServer().getWorld(args[0]).setThundering(false);
-                    plugin.getServer().getWorld(args[0]).setStorm(true);
-                    sender.sendMessage("Weather set to rainy in " + plugin.getServer().getWorld(args[0]).getName() + "!");
-                    return false;
-                } else {
-                    sender.sendMessage("World not found.");
-                    return false;
-                }
-            } else if(args.length == 0) {
-                sender.sendMessage("World not given.");
-                return false;
-            } else {
-                sender.sendMessage("Too many arguments.");
-                return false;
+            if(args.length == 0) {
+                sender.sendMessage(plugin.getMessage("weather.usage", "rain"));
+                return true;
             }
+            if(plugin.getServer().getWorld(args[0]) == null) {
+                sender.sendMessage(plugin.getMessage("weather.world_not_found", args[0]));
+                return true;
+            }
+            plugin.getServer().getWorld(args[0]).setThundering(false);
+            plugin.getServer().getWorld(args[0]).setStorm(true);
+            sender.sendMessage(plugin.getMessage("weather.set2", "rainy", args[0]));
+            return true;
         }
 
         //Set weather to stormy
         if(command.getName().equalsIgnoreCase("storm")) {
             if(sender instanceof Player) {
                 if(!sender.hasPermission("aio.weather")) {
-                    sender.sendMessage("You don't have permission to execute this command.");
-                    return false;
+                    sender.sendMessage(plugin.getMessage("messages.no_permission"));
+                    return true;
                 }
                 if(args.length == 0) {
                     ((Player) sender).getLocation().getWorld().setThundering(true);
                     ((Player) sender).getLocation().getWorld().setStorm(true);
-                    sender.sendMessage("Weather set to stormy!");
-                    return false;
+                    sender.sendMessage(plugin.getMessage("weather.set1", "stormy"));
+                    return true;
                 }
             }
-            if(args.length == 1) {
-                if(plugin.getServer().getWorld(args[0]) != null) {
-                    plugin.getServer().getWorld(args[0]).setThundering(true);
-                    plugin.getServer().getWorld(args[0]).setStorm(true);
-                    sender.sendMessage("Weather set to stormy in " + plugin.getServer().getWorld(args[0]).getName() + "!");
-                    return false;
-                } else {
-                    sender.sendMessage("World not found.");
-                    return false;
-                }
-            } else if(args.length == 0) {
-                sender.sendMessage("World not given.");
-                return false;
-            } else {
-                sender.sendMessage("Too many arguments.");
-                return false;
+            if(args.length == 0) {
+                sender.sendMessage(plugin.getMessage("weather.usage", "storm"));
+                return true;
             }
+            if(plugin.getServer().getWorld(args[0]) == null) {
+                sender.sendMessage(plugin.getMessage("weather.world_not_found", args[0]));
+                return true;
+            }
+            plugin.getServer().getWorld(args[0]).setThundering(true);
+            plugin.getServer().getWorld(args[0]).setStorm(true);
+            sender.sendMessage(plugin.getMessage("weather.set2", "stormy", args[0]));
+            return true;
         }
 
         return false;
