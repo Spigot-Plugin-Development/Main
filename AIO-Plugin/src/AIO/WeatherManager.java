027eb6d327fb705abd3695254bdda98288700223
Ádám András <adm.andras@gmail.com>
2018-07-13 14:26:29 +0200

    New weather manager

--- AIO-Plugin/src/AIO/WeatherManager.java
+++ AIO-Plugin/src/AIO/WeatherManager.java
@@ -6,98 +6,62 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
+
 public class WeatherManager implements CommandExecutor {
     private aio plugin;
+
+    private HashMap<String, String> weatherList = new HashMap<>();
 
     WeatherManager(aio plugin) {
         this.plugin = plugin;
         Bukkit.getServer().getPluginCommand("sun").setExecutor(this);
         Bukkit.getServer().getPluginCommand("rain").setExecutor(this);
         Bukkit.getServer().getPluginCommand("storm").setExecutor(this);
+
+        weatherList.put("sun", "sunny");
+        weatherList.put("rain", "rainy");
+        weatherList.put("storm", "stormy");
+    }
+
+    private void setWeather(String weather, String world) {
+        if(weather.equalsIgnoreCase("sun")) {
+            plugin.getServer().getWorld(world).setThundering(false);
+            plugin.getServer().getWorld(world).setStorm(false);
+        }
+        if(weather.equalsIgnoreCase("rain")) {
+            plugin.getServer().getWorld(world).setThundering(false);
+            plugin.getServer().getWorld(world).setStorm(true);
+        }
+        if(weather.equalsIgnoreCase("storm")) {
+            plugin.getServer().getWorld(world).setThundering(true);
+            plugin.getServer().getWorld(world).setStorm(true);
+        }
     }
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        //Set weather to sunny
-        if(command.getName().equalsIgnoreCase("sun")) {
+        if(weatherList.containsKey(command.getName())) {
             if(sender instanceof Player) {
                 if(!sender.hasPermission("aio.weather")) {
-                    sender.sendMessage(plugin.getMessage("messages.no_permission"));
+                    sender.sendMessage(plugin.getMessage("aio.no_permission"));
                     return true;
                 }
                 if(args.length == 0) {
-                    ((Player) sender).getLocation().getWorld().setThundering(false);
-                    ((Player) sender).getLocation().getWorld().setStorm(false);
-                    sender.sendMessage(plugin.getMessage("weather.set1", "sunny"));
+                    setWeather(command.getName(), ((Player) sender).getWorld().getName());
+                    sender.sendMessage(plugin.getMessage("weathermanager.set_self", weatherList.get(command.getName())));
                     return true;
                 }
             }
             if(args.length == 0) {
-                sender.sendMessage(plugin.getMessage("weather.usage", "sun"));
+                sender.sendMessage(plugin.getMessage("weathermanager.usage", command.getName()));
                 return true;
             }
             if(plugin.getServer().getWorld(args[0]) == null) {
-                sender.sendMessage(plugin.getMessage("weather.world_not_found", args[0]));
+                sender.sendMessage(plugin.getMessage("aio.world_not_found", args[0]));
                 return true;
             }
-            plugin.getServer().getWorld(args[0]).setThundering(false);
-            plugin.getServer().getWorld(args[0]).setStorm(false);
-            sender.sendMessage(plugin.getMessage("weather.set2", "sunny", args[0]));
-            return true;
-        }
-
-        //Set weather to rainy
-        if(command.getName().equalsIgnoreCase("rain")) {
-            if(sender instanceof Player) {
-                if(!sender.hasPermission("aio.weather")) {
-                    sender.sendMessage(plugin.getMessage("messages.no_permission"));
-                    return true;
-                }
-                if(args.length == 0) {
-                    ((Player) sender).getLocation().getWorld().setThundering(false);
-                    ((Player) sender).getLocation().getWorld().setStorm(true);
-                    sender.sendMessage(plugin.getMessage("weather.set1", "rainy"));
-                    return true;
-                }
-            }
-            if(args.length == 0) {
-                sender.sendMessage(plugin.getMessage("weather.usage", "rain"));
-                return true;
-            }
-            if(plugin.getServer().getWorld(args[0]) == null) {
-                sender.sendMessage(plugin.getMessage("weather.world_not_found", args[0]));
-                return true;
-            }
-            plugin.getServer().getWorld(args[0]).setThundering(false);
-            plugin.getServer().getWorld(args[0]).setStorm(true);
-            sender.sendMessage(plugin.getMessage("weather.set2", "rainy", args[0]));
-            return true;
-        }
-
-        //Set weather to stormy
-        if(command.getName().equalsIgnoreCase("storm")) {
-            if(sender instanceof Player) {
-                if(!sender.hasPermission("aio.weather")) {
-                    sender.sendMessage(plugin.getMessage("messages.no_permission"));
-                    return true;
-                }
-                if(args.length == 0) {
-                    ((Player) sender).getLocation().getWorld().setThundering(true);
-                    ((Player) sender).getLocation().getWorld().setStorm(true);
-                    sender.sendMessage(plugin.getMessage("weather.set1", "stormy"));
-                    return true;
-                }
-            }
-            if(args.length == 0) {
-                sender.sendMessage(plugin.getMessage("weather.usage", "storm"));
-                return true;
-            }
-            if(plugin.getServer().getWorld(args[0]) == null) {
-                sender.sendMessage(plugin.getMessage("weather.world_not_found", args[0]));
-                return true;
-            }
-            plugin.getServer().getWorld(args[0]).setThundering(true);
-            plugin.getServer().getWorld(args[0]).setStorm(true);
-            sender.sendMessage(plugin.getMessage("weather.set2", "stormy", args[0]));
+            setWeather(command.getName(), args[0]);
+            sender.sendMessage(plugin.getMessage("weathermanager.set_world", weatherList.get(command.getName()), args[0]));
             return true;
         }
 
